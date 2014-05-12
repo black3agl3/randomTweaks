49110cf7ff197efb2ca8fce44f0d1bcd4344e87b
black3agl3 <black_3agl3@live.com>
2014-05-12 04:07:31 +0400

    Helper for registering Block and Item
    Extended Helper class
    Cleaned up Info class

--- java/com/black3agl3/randomtweaks/Helper.java
+++ java/com/black3agl3/randomtweaks/Helper.java
@@ -1,16 +1,43 @@
 package com.black3agl3.randomtweaks;
 
+import org.omg.CORBA.portable.UnknownException;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 public class Helper {
-	public static void preInit(){
-		
+	
+	public static void preInit () {
+	
 	}
 	
-	public static void init(){
-		
+	public static void init () {
+	
 	}
 	
-	public static void postInit(){
+	public static void postInit () {
+	
+	}
+	
+	public static void register (Object obj) {
+	
+		if (obj instanceof Block) {
+			Block block = (Block)obj;
+			GameRegistry.registerBlock(block, unwrapUnlocalizedName(block.getUnlocalizedName()));
+		} 
+		else if (obj instanceof Item) {
+			Item item = (Item)obj;
+			GameRegistry.registerItem(item, unwrapUnlocalizedName(item.getUnlocalizedName()));
+		}
+		else throw new UnknownException(null);
+	}
+	
+	private static String unwrapUnlocalizedName(String unlocalizedName){
+		int startindex = unlocalizedName.indexOf('.') + 1;
+		int endindex = unlocalizedName.indexOf('.', startindex);
 		
+		if (endindex<0) return unlocalizedName.substring(startindex);
+		else return unlocalizedName.substring(startindex, endindex);
 	}
 }
--- java/com/black3agl3/randomtweaks/Info.java
+++ java/com/black3agl3/randomtweaks/Info.java
@@ -1,7 +1,9 @@
 package com.black3agl3.randomtweaks;
 
 public class Info {
-	public static final String MODID = "randomtweaks";
-	public static final String NAME = "Random Tweaks";
-	public static final String VERSION = "1.0";
+	public static final String 
+			MODID = "randomtweaks",
+			NAME = "Random Tweaks",
+			VERSION = "1.0"
+			;
 }
